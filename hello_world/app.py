25fc430510b1b3f6c5e32c2f853efb0dd8704334
Mark Stouffer <1802850+InTEGr8or@users.noreply.github.com>
2022-12-25 23:17:41 -0800

    Saving video also

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,22 +1,61 @@
 import json
+import io
+from datetime import datetime
 from pytube import YouTube
 import boto3
 import sys
 import logging
 import os.path
 import re
-# import xmltodict
+import xmltodict
+from functools import reduce
 
 S3 = boto3.client('s3', region_name="us-east-1")
 vidIdPattern = re.compile(r"((?<=watch\?v=).*|(?<=.be\/)(.*))")
+dyndb = boto3.client('dynamodb', region_name="us-east-1")
+
+def log_video(videoUrl, property):
+    try:
+        videoId = vidIdPattern.search(videoUrl).group()
+        item = {
+            "VideoId": {"S": videoId},
+            "VideoUrl": {"S": videoUrl}
+        }
+        if property:
+            item[property['name']] = property['value']
+        dyndb.put_item(
+            TableName="videos",
+            Item=item
+        )
+    except Exception as e:
+        logging.error(dir(videoUrl), e)
+        return 1
 
 def get_captions (videoUrl):
     try:
         captions = YouTube(videoUrl).captions['a.en']
         return captions.xml_captions
     except Exception as e:
-        logger.error(dir(videoUrl), e)
+        logging.error(dir(videoUrl), e)
         return 1
+
+def xml_to_csv(xml):
+    caption_obj = xmltodict.parse(xml)['timedtext']['body']['p']
+    captions_csv = ""
+    captions = [c for c in caption_obj if 's' in c ]
+    for caption in captions:
+        time = caption['@t']
+        text_list = []
+        # if there is only one word in the caption
+        # it will be a dict instead of a list
+        # so we need to convert it to a list
+        if type(caption['s']) is dict:
+            caption['s'] = [caption['s']]
+        for si in caption['s']:
+            text_list.append(si['#text'])
+        text = ' '.join(text_list)
+        captions_csv += (f"{time}, {text}\n")
+    return captions_csv
 
 def get_video (videoUrl):
     try:
@@ -24,67 +63,43 @@ def get_video (videoUrl):
         progressive=True, subtype='mp4',
     ).order_by('filesize').desc().first()
     except Exception as e:
-        logger.error(dir(videoUrl), e)
+        logging.error(dir(videoUrl), e)
         return
 
-    dl_stream = yt
+    buf = io.BytesIO()
     try:
-        captions = dl_stream.player_config_args['player_response'][
-            'captions']['playerCaptionsTracklistRenderer']['captionTracks']
-        english_captions = next(
-            (c for c in captions if c['languageCode'] == 'en'), None)
-        englishCaptionsUrl = english_captions['baseUrl']
-        file = urllib.request.urlopen(englishCaptionsUrl)
-        data = file.read()
-        file.close()
-        print("CAPTIONS FOR:", caption_path)
-
-        data = xmltodict.parse(data)
-        if data:
-
-            transcript = '\n'.join(line['@start'] + ': ' + line['#text'] for line in data["transcript"]["text"])
-            file_stream = open(caption_path, "w")
-            file_stream.close()
-        return 0
+        yt.stream_to_buffer(buf)
+        buf.seek(0)
+        videoData = buf.read()
+        return videoData
     except Exception as e:
-        logger.info("NO CAPTIONS\n")
-
-
+        logging.info("NO CAPTIONS\n")
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
+    logging.info("HANDLER EVENT:", event)
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
     videoUrl = event['videoUrl']
     videoId = vidIdPattern.search(videoUrl).group()
+    log_video(videoUrl, None)
     xml_captions = get_captions(videoUrl)
+    
+    logging.info("WRIGING XML", event)
+
+
     captionFile = f"captions/{videoId}.xml"
     S3.put_object(Bucket="civilton-youtube-content", Key=captionFile, Body=xml_captions)
+    log_video(event['videoUrl'], {"name": "xml_captions", "value": {"S": datetime.now().strftime("%Y-%m-%d %H:%M:%S")}})
+
+    csv_captions = xml_to_csv(xml_captions)
+    captionFile = f"captions/{videoId}.csv"
+    S3.put_object(Bucket="civilton-youtube-content", Key=captionFile, Body=csv_captions)
+    log_video(event['videoUrl'], {"name": "csv_captions", "value": {"S": datetime.now().strftime("%Y-%m-%d %H:%M:%S")}})
+
+    videoData = get_video(videoUrl)
+    videoFIle = f"videos/{videoId}.mp4"
+    S3.put_object(Bucket="civilton-youtube-content", Key=videoFIle, Body=videoData)
+    log_video(event['videoUrl'], {"name": "video_file", "value": {"S": datetime.now().strftime("%Y-%m-%d %H:%M:%S")}})
+
     return {
         "statusCode": 200,
         "body": json.dumps({
@@ -92,3 +107,6 @@ def lambda_handler(event, context):
             # "location": ip.text.replace("\n", "")
         }),
     }
+
+if len(sys.argv) > 1:
+    lambda_handler(json.loads(sys.argv[1]), None)
