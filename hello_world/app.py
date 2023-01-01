ab22da76a86d6a4e556e5da10e04035dc5cc142b
Mark Stouffer <1802850+InTEGr8or@users.noreply.github.com>
2023-01-01 11:21:52 -0800

    Include audio

--- hello_world/app.py
+++ hello_world/app.py
@@ -22,6 +22,11 @@ URL_EXPIRATION = [config for config in configs if 'url_expiration' in config][0]
 CHAT_GPT_TOKEN = [config for config in configs if 'CHAT_GPT_TOKEN' in config][0].split("=")[1]
 MAX_TOKENS = int([config for config in configs if 'max_tokens' in config][0].split("=")[1])
 TEMPERATURE = float([config for config in configs if 'temperature' in config][0].split("=")[1])
+ENGINE = [config for config in configs if 'engine' in config][0].split("=")[1]
+
+MAX_REQUEST_TOKENS = 4000
+
+openai.api_key = CHAT_GPT_TOKEN
 
 # Logger writes to CloudWatch
 logger = logging.getLogger()
@@ -41,44 +46,72 @@ class Property:
         self.type = type
         self.value = value
 
-def send_summary_request(text: str):
+def split_text(text: str, max_chars: int=2049) -> list:
+
+    words = text.split()
+    groups = []
+    current_group = ""
+
+    for word in words:
+        if len(current_group) < max_chars:
+            current_group += " " + word
+        else:
+            groups.append(current_group[1:])
+            current_group = " " + word
+
+    return groups
+
+def get_summary_stream(text: str, max_tokens, temperature=0.9, engine="davinci") -> str:
     # Use the openai.Completion.stream() method to stream the text to the GPT-3 model
     completion = openai.Completion.stream(
-        model="text-davinci-002",
-        temperature=0.7, # controls the creativity of the summary
-        max_tokens=100, # maximum number of tokens in the summary
+        model=engine,
+        temperature=temperature, # controls the creativity of the summary
+        max_tokens=max_tokens, # maximum number of tokens in the summary
     )
 
     # Send the text to the GPT-3 model in chunks of 2049 tokens or fewer
-    for i in range(0, len(text), 2049):
-        chunk = text[i:i+2049]
+    text = f"Split into sentences & summarize the following text in {max_tokens} tokens: \n\n[{text}]"
+    for i in range(0, len(text), MAX_REQUEST_TOKENS):
+        chunk = text[i:i+MAX_REQUEST_TOKENS]
         completion.add_input(prompt=chunk)
 
     # Get the summary from the GPT-3 model
     summary = completion.get_response()["choices"][0]["text"]
 
-def get_summary(text: str) -> str:
-    max_total_tokens = 2049
-    chunks = [text[i:i+max_total_tokens] for i in range(0, len(text), max_total_tokens)]
-    max_summary_tokens = int(MAX_TOKENS / len(chunks))
+def get_summary_response(text: str, max_tokens, temperature=0.9, engine="davinci") -> str:
+    # TODO: WRONG! Summary must be split by words, or better yet, sentences
+
+    chunks = split_text(text, max_chars=(MAX_REQUEST_TOKENS - 83))
+    max_summary_tokens = int(max_tokens / len(chunks))
     summaries = []
-    for chunck in chunks:
+    for chunk in chunks:
+        prompt_chunk = f"Split into sentences & summarize the following text in {max_summary_tokens} tokens: \n\n[{chunk}]"
         response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=text,
+            engine=engine,
+            prompt=prompt_chunk,
             max_tokens=max_summary_tokens,
-            temperature=TEMPERATURE,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0.6,
-            stop=["\n", " Human:", " AI:"]
+            temperature=temperature,
+            top_p=1
         )
+        print("RESPONSE")
+        print(response)
         summaries.append(response.choices[0].text)
     summary = " ".join(summaries)
     return summary
 
+def get_summary(text: str, max_tokens, temperature=0.9, engine="davinci") -> str:
+    if len(text) > 1000000000: #MAX_REQUEST_TOKENS:
+        return get_summary_stream(text, max_tokens, temperature, engine)
+    else:
+        return get_summary_response(text, max_tokens, temperature, engine)
+
 
 def update_video_table(videoUrl, properties) -> int:
+    videoId = vidIdPattern.search(videoUrl).group()
+    item = {
+        "VideoId": {"S": videoId},
+        "VideoUrl": {"S": videoUrl}
+    }
     try:
         videoId = vidIdPattern.search(videoUrl).group()
         item = {
@@ -91,7 +124,14 @@ def update_video_table(videoUrl, properties) -> int:
                 Item=item
             )
             return 0
+    except Exception as e:
+        logger.error('put_video_table ERROR')
+        logger.error(e)
+        logger.error('VIDEO_URL')
+        logger.error(dir(videoUrl))
+        return 1
 
+    try:
         if type(properties) is dict:
             properties = [properties]
         attributeValues = {}
@@ -114,6 +154,7 @@ def update_video_table(videoUrl, properties) -> int:
     except Exception as e:
         logger.error('update_video_table ERROR')
         logger.error(e)
+        logger.error(attributeValues)
         logger.error('VIDEO_URL')
         logger.error(dir(videoUrl))
         return 1
@@ -125,16 +166,17 @@ def get_captions (videoUrl: str) -> str:
         yt = YouTube(videoUrl)
         logger.info("GET_CAPTION")
         logger.info(len(yt.captions))
+        title = yt.title
         description = yt.description
         thumbnail_url = yt.thumbnail_url
         keywords = yt.keywords
         use_oauth = yt.use_oauth
         captions = yt.captions['a.en'].xml_captions if 'a.en' in yt.captions else ''
-        return captions, description, thumbnail_url, keywords, use_oauth
+        return captions, description, thumbnail_url, keywords, use_oauth, title
     except Exception as e:
         logger.error("GET_CAPTION_error")
         logger.error(e)
-        return captions, description, thumbnail_url, keywords, use_oauth
+        return captions, description, thumbnail_url, keywords, use_oauth, title
 
 def xml_to_csv(xml):
     if(len(xml) < 30):
@@ -165,6 +207,28 @@ def is_head_object(Bucket, Key):
     except:
         return False
 
+def get_audio (videoUrl) -> Result:
+    try:
+        yt = YouTube(videoUrl).streams
+        audio_streams = list(filter(lambda fs:fs.type=='audio', yt.fmt_streams))
+        audio_sorted = list(sorted(audio_streams, key=lambda au:au.filesize, reverse=True))
+        audio_stream = audio_sorted[2]
+    except Exception as e:
+        logger.error("GET_AUDIO_ERROR")
+        logger.error(e)
+        return Result(500, error="Error getting audio")
+
+    buf = io.BytesIO()
+    try:
+        audio_stream.stream_to_buffer(buf)
+        buf.seek(0)
+        audioData = buf.read()
+        return Result(200, message="Total bytes saved: " + str(len(audioData)), body=audioData) 
+    except Exception as e:
+        logger.info("GET_AUDIO_ERROR - Buffer read error")
+        logger.error(e)
+        return Result(500, error="Buffer read error")
+
 def get_video (videoUrl) -> Result:
     try:
         yt = YouTube(videoUrl).streams.filter(
@@ -192,6 +256,7 @@ def lambda_handler(event, context):
 
     # Get Video URL
     # It will be in different places depending on how the lambda is invoked
+    replace_summary = True if('replaceSummary' in event and event['replaceSummary'] == 'true') else False
     if('videoUrl' in event): # Lambda test invoke
         videoUrl = event['videoUrl']
     elif('queryStringParameters' in event and event['queryStringParameters'] and 'videoUrl' in event['queryStringParameters']): # GET request with query string or API Gateway proxy tester
@@ -205,17 +270,18 @@ def lambda_handler(event, context):
     videoId = vidIdPattern.search(videoUrl).group()
     update_video_table(videoUrl, None)
 
-    # Get Captions
-
+    # Get Captions and Description and metadata
     logger.info("GETTING XML CAPTIONS AND DESCRIPTION")
-    xml_captions, description, thumbnail_url, keywords, use_oauth = get_captions(videoUrl)
+    xml_captions, description, thumbnail_url, keywords, use_oauth, title = get_captions(videoUrl)
     logger.info("WRITING XML and DESCRIPTION TO S3")
 
+    # Write captions to S3
     if len(xml_captions) > 30:
-        captionFile = f"/{videoId}/captions.xml"
+        captionFile = f"/{videoId}/{videoId}_captions.xml"
         if not is_head_object(Bucket=BUCKET_NAME, Key=captionFile):
             S3.put_object(Bucket=BUCKET_NAME, Key=captionFile, Body=xml_captions)
 
+    # Write description and metadata to DynamoDB
     update_video_table(videoUrl, [
             {
                 "name": "xml_captions", 
@@ -235,33 +301,40 @@ def lambda_handler(event, context):
             },
             {
                 "name": "keywords",
-                "value": {"S": keywords}
+                "value": {"S": ",".join(keywords) if keywords.__class__ == list else keywords}
             },
             {
                 "name": "use_oauth",
-                "value": {"S": use_oauth}
+                "value": {"BOOL": use_oauth}
             }
         ]
     )
 
+    # Create CSV captions and summary
+    summary = ""
+    txt_captions = ""
     if len(xml_captions) > 30:
         logger.info("## CCONVERT XML CAPTIONS TO CSV AND WRITING CSV")
         csv_captions, txt_captions = xml_to_csv(xml_captions)
-        captionFile = f"/{videoId}/captions.csv"
-        summaryFile = f"/{videoId}/summary.txt"
+        captionFile = f"/{videoId}/{videoId}_captions.csv"
+        summaryFile = f"/{videoId}/{videoId}_summary.txt"
 
         if not is_head_object(Bucket=BUCKET_NAME, Key=captionFile):
             S3.put_object(Bucket=BUCKET_NAME, Key=captionFile, Body=csv_captions)
-        if not is_head_object(Bucket=BUCKET_NAME, Key=summaryFile):
-            summary_txt = get_summary(text=txt_captions)
-            if summary_txt and len(summary_txt) > 0:
-                S3.put_object(Bucket=BUCKET_NAME, Key=summaryFile, Body=summary_txt)
+
+        # Create summary or get summary from S3
+        if is_head_object(Bucket=BUCKET_NAME, Key=summaryFile) and not replace_summary:
+            summary = S3.get_object(Bucket=BUCKET_NAME, Key=summaryFile)['Body'].read().decode('utf-8')
+        else:
+            summary = get_summary(text=txt_captions, max_tokens=MAX_TOKENS, temperature=TEMPERATURE, engine=ENGINE)
+            if summary and len(summary) > 0:
+                S3.put_object(Bucket=BUCKET_NAME, Key=summaryFile, Body=summary)
 
         update_video_table(videoUrl, {"name": "csv_captions", "value": {"S": datetime.now().strftime("%Y-%m-%d %H:%M:%S")}})
 
     # Get Video
     logger.info("## GETTING VIDEO AND WRITING Video File")
-    videoFile = f"/{videoId}/videos.mp4"
+    videoFile = f"/{videoId}/{videoId}_videos.mp4"
 
     # Check if video already exists
     if is_head_object(Bucket=BUCKET_NAME, Key=videoFile):
@@ -274,8 +347,25 @@ def lambda_handler(event, context):
             # Put Video
             S3.put_object(Bucket=BUCKET_NAME, Key=videoFile, Body=videoData)
 
-    presigned_url = S3.generate_presigned_url('get_object', Params={'Bucket': BUCKET_NAME, 'Key': videoFile}, ExpiresIn=URL_EXPIRATION)
+    video_presigned_url = S3.generate_presigned_url('get_object', Params={'Bucket': BUCKET_NAME, 'Key': videoFile}, ExpiresIn=URL_EXPIRATION)
 
+    # Get audio
+    logger.info("## GETTING audio AND WRITING audio File")
+    audioFile = f"/{videoId}/{videoId}_audio.mp3"
+
+    # Check if audio already exists
+    if is_head_object(Bucket=BUCKET_NAME, Key=audioFile):
+        audioDataResult = Result(statusCode=200, message="audio already exists")
+    else:
+        # GET audio
+        audioDataResult = get_audio(videoUrl)
+        if audioDataResult and audioDataResult.statusCode == 200:
+            audioData = audioDataResult.body
+            # Put audio
+            S3.put_object(Bucket=BUCKET_NAME, Key=audioFile, Body=audioData)
+
+    audio_presigned_url = S3.generate_presigned_url('get_object', Params={'Bucket': BUCKET_NAME, 'Key': audioFile}, ExpiresIn=URL_EXPIRATION)
+    
     # Update Video Table
     update_video_table(videoUrl, [
         {
@@ -284,13 +374,17 @@ def lambda_handler(event, context):
         },
         {
             "name": "video_status",
-            "value": {"S": videoDataResult.statusCode if videoDataResult else "400"}
+            "value": {"S": str(videoDataResult.statusCode) if videoDataResult else "400"}
         }
     ])
 
     return {
-        "statusCode": 200,
-        "body": videoDataResult.message + "\nPresigned URL: " + presigned_url if videoDataResult and videoDataResult.statusCode == 200 else  videoDataResult.error if videoDataResult else "Error getting video"
+        "statusCode": 200 if videoDataResult and videoDataResult.statusCode == 200 else 400,
+        "body": """{videoDataResult.message}
+        Presigned VIDEO URL: {video_presigned_url}
+        Presigned AUDIO URL: {audio_presigned_url}
+        SUMMARY OF VIDEO: {summary}
+        """ if videoDataResult and videoDataResult.statusCode == 200 else  videoDataResult.error if videoDataResult else "Error getting video"
    }
 
 if len(sys.argv) > 1:
